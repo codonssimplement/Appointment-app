6883a093cd62a278c96a1dc94a37115681699cab
codonssimplement <118065712+codonssimplement@users.noreply.github.com>
2025-05-27 15:44:51 +0000

    Update PatientPage.jsx

--- src/components/PatientPage.jsx
+++ src/components/PatientPage.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { FaBell } from "react-icons/fa";
+//import { FaBell } from "react-icons/fa";
 import { LuPencilLine } from "react-icons/lu";
 import profil from "../assets/images/profil.png";
 import Menu from "./Menu";
